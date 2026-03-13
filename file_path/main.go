e7b2bd3506ab0267dde18f4bac637ec85df92f52
kzhk75 <u.kzhk75@gmail.com>
2026-03-13 17:26:46 +0900

    Shift towards Walis

--- file_path/main.go
+++ file_path/main.go
@@ -1,52 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"path/filepath"
+	"embed"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
+//go:embed all:frontend/dist
+var assets embed.FS
+
 func main() {
-	// 1. アプリとウィンドウの作成
-	a := app.New()
-	w := a.NewWindow("成績資料PDFコンバーター")
+	// Create an instance of the app structure
+	app := NewApp()
 
-	// 2. 画面に表示するテキスト
-	statusLabel := widget.NewLabel("ここにWordファイルをドラッグ＆ドロップしてください")
-	statusLabel.Alignment = fyne.TextAlignCenter
-
-	// 3. ウィンドウにファイルがドロップされた時の処理
-	w.SetOnDropped(func(_ fyne.Position, uris []fyne.URI) {
-		// 複数のファイルがドロップされても1つずつ処理
-		for _, uri := range uris {
-			inputPath := uri.Path()
-			
-			// 拡張子がWordかどうか簡易チェック
-			ext := filepath.Ext(inputPath)
-			if ext != ".doc" && ext != ".docx" {
-				statusLabel.SetText("エラー: Wordファイル以外がドロップされました\n" + inputPath)
-				continue
-			}
-
-			statusLabel.SetText(fmt.Sprintf("処理中...\n%s", inputPath))
-
-			// --------------------------------------------------------
-			// ★ここに先ほど完成した ConvertWordToPDF 関数を呼び出す処理を書きます★
-			// 例: err := ConvertWordToPDF(inputPath, outputPath)
-			// --------------------------------------------------------
-
-			statusLabel.SetText(fmt.Sprintf("✅ 変換完了！\n%s", inputPath))
-		}
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "renamer",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
 	})
 
-	// 4. 画面のレイアウトとサイズ調整
-	w.SetContent(container.NewCenter(statusLabel))
-	w.Resize(fyne.NewSize(500, 300))
-
-	// 5. アプリの実行
-	w.ShowAndRun()
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
